60aeec5d3ee5d9444e62acef6c9f42ad75790ccf
pythontoday <pythontodaynet@gmail.com>
2021-10-22 17:31:27 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Text Recognition
+### 🎥 [PYTHON:TODAY](https://www.youtube.com/c/PythonToday/videos)
+### 🔥 [Telegram](https://t.me/python2day)
+---
+### pip install easyocr or pip install -r requirements.txt
+---
+
+### Use
+```
+1. python main.py
+```
+```
+2. Enter an image file path
+```
---
+++ main.py
@@ -0,0 +1,22 @@
+# pip install easyocr or pip install -r requirements.txt
+import easyocr
+
+
+def text_recognition(file_path, text_file_name="result.txt"):
+    reader = easyocr.Reader(["ru", "en"])
+    result = reader.readtext(file_path, detail=0, paragraph=True)
+    
+    with open(text_file_name, "w") as file:
+        for line in result:
+            file.write(f"{line}\n\n")
+    
+    return f"Result wrote into {text_file_name}"
+
+
+def main():
+    file_path = input("Enter a file path: ")
+    print(text_recognition(file_path=file_path))
+    
+    
+if __name__ == "__main__":
+    main()
